6cdacc9231e26dbea6a3561da9cb3cdd09b52a89
UniSoloGr <filipp.barkov@helsinki.fi>
2026-05-14 18:20:59 +0300

    feat: 1b osa valmis

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,53 +1,61 @@
 const Header = (props) => {
-  console.log(props)
+  console.log(props.course)
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   )
 }
 
 const Content = (props) => {
-  console.log(props)
   return (
-  <>
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  </>
+    <>
+      <p>
+        {props.course.parts[0].name} {props.course.parts[0].exercises}
+      </p>
+      <p>
+        {props.course.parts[1].name} {props.course.parts[1].exercises}
+      </p>
+      <p>
+        {props.course.parts[2].name} {props.course.parts[2].exercises}
+      </p>
+    </>
   )
 }
 
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+      <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  console.log(course)
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1}/>
-      <Content part={part2}/>
-      <Content part={part3}/>
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
@@ -59,6 +67,5 @@ const Hello = () => {
     </div>
   )
 }
-
 
 export default App
