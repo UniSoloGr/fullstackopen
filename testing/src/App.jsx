e7ba34bba93f869ac6801c185e1ad1eec0699b82
UniSoloGr <filipp.barkov@helsinki.fi>
2026-05-15 15:06:28 +0300

    feat: create multiple projects for part1

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -2,33 +2,70 @@ import {useState} from "react"
 
 const Button = (props) => {
   return (
-  <button onClick={props.onClick}>
-    {props.text}
-  </button>
+  <button onClick={props.onClick} />
   )
 }
 
-const CompactButton = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
+const CompactButton = ({ onClick, text}) => <button onClick={onClick}></button>
 
 const Counter = (props) => {
   return (
-  <div>{props.counter}</div>
+  <div>{props.counter} {props.name} </div>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app has not been initialized
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(" ")}
+    </div>
   )
 }
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [ left, setLeft ] = useState(0)
+  const [ right , setRight ] = useState(0)
+  const [ allClicks, setAll ] = useState([])
+  const [ total, setTotal ] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = (newLeft) => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = newLeft
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + left)
+  }
+
+  const setToValue = (newValue) => () => {
+    setRight(newValue)
+  }
 
   return (
     <div>
-      <Counter counter={counter} />
-      <Button onClick={increaseByOne} text='+1' />
-      <CompactButton onClick={decreaseByOne} text='-1' />
-      <Button onClick={setToZero} text='0' />
+      left {left}
+      <CompactButton onClick={() => handleLeftClick(left + 1)}> left </CompactButton>
+      <CompactButton onClick={handleRightClick}> right </CompactButton>
+      <CompactButton onClick={setToValue(1000)}> right </CompactButton>
+      <button onClick={setToValue(0)}>set to 0</button>
+      <button onClick={() => handleLeftClick(0)}>set left to 0</button>
+      <button onClick={() => handleLeftClick(left + 1)}>left + 1</button>
+      right {right}
+      <History allClicks={allClicks} />
+      <p>
+        {total} : Total
+      </p>
     </div>
   )
 }
