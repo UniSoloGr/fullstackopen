d16f9a2e740971b8f530fc93ef0f9899bf962a59
UniSoloGr <filipp.barkov@helsinki.fi>
2026-06-18 12:30:35 +0300

    feat: do exercises 3.15-3.16

--- part3/puhelinluettelo/backend/index.js
+++ part3/puhelinluettelo/backend/index.js
@@ -1,16 +1,35 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
-const Person = require('./models/person')
-
 const apiPersons= "/api/persons"
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
 
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(cors())
+app.use(requestLogger)
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello world!</h1>')
@@ -29,7 +48,7 @@ app.get('/info', (request, response) => {
   `)
 })
 
-app.get(apiPersons + '/:id', (request, response) => {
+app.get(apiPersons + '/:id', (request, response, next) => {
   const id = request.params.id
   Person.findById(id)
     .then((person) => {
@@ -39,22 +58,23 @@ app.get(apiPersons + '/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
-app.post(apiPersons, (request, response) => {
+app.post(apiPersons, (request, response, next) => {
   const errorMessage = (message) => {
     return response.status(400).json({
       error: `${message}`
     })
   }
-  console.log("request", request)
   const body = request.body
 
   if (!body.name) {
-    errorMessage(`name missing`)
-  } else if (!body.number) {
-    errorMessage('number missing')
+    return errorMessage(`name missing`)
+  } 
+
+  if (!body.number) {
+    return errorMessage('number missing')
   } 
 
   const person = new Person({
@@ -62,14 +82,15 @@ app.post(apiPersons, (request, response) => {
     number: body.number
   })
 
-  console.log(body)
 
-  person.save().then((savedPerson) => {
+  person.save()
+    .then((savedPerson) => {
     response.json(savedPerson)
-  })
+    })
+    .catch(error => next(error))
 })
 
-app.delete(apiPersons + '/:id', (request, response) => {
+app.delete(apiPersons + '/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndDelete(id)
     .then((result) => {
@@ -78,9 +99,12 @@ app.delete(apiPersons + '/:id', (request, response) => {
     .catch((error) => next(error))
 })
 
-const generateId = (max) => {
-  return Math.floor(Math.random() * max)
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
