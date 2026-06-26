ebd5950959f8d60d05ac28afcc9a9aef765eea84
UniSoloGr <filipp.barkov@helsinki.fi>
2026-06-26 14:13:28 +0300

    feat: do exercise 3.22

--- part3/puhelinluettelo/backend/models/person.js
+++ part3/puhelinluettelo/backend/models/person.js
@@ -5,29 +5,29 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 mongoose
-    .connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const phonebookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
-    },
-    number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: String,
 })
 
 phonebookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    },
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports = mongoose.model('Person', phonebookSchema)
--- part3/puhelinluettelo/backend/mongo.js
+++ part3/puhelinluettelo/backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,47 +11,47 @@ const url = `mongodb+srv://raceer:${password}@klusteri.6u46jbz.mongodb.net/phone
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
+  name: String,
+  phone: String,
 })
 
 const Person = mongoose.model('Person', phonebookSchema)
 
 const newEntry = (name, phone) => {
-    const note = new Person({
-        name: name, 
-        phone: phone,
-    })
+  const note = new Person({
+    name: name,
+    phone: phone,
+  })
 
-    note.save().then(result => {
-        console.log('note saved!')
-        console.log(result)
-        mongoose.connection.close()
-    })
+  note.save().then(result => {
+    console.log('note saved!')
+    console.log(result)
+    mongoose.connection.close()
+  })
 }
 
 const getAllEntries = () => {
-    Person.find({}).then(result => {
-        result.forEach(note => {
-            console.log(note)
-        })
-        mongoose.connection.close()
-    })    
+  Person.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
 }
 
 if (process.argv.length === 5) {
-    const name = process.argv[3]
-    const phone = process.argv[4]
-    newEntry(name, phone)
+  const name = process.argv[3]
+  const phone = process.argv[4]
+  newEntry(name, phone)
 } else if (process.argv.length === 3) {
-    getAllEntries()
+  getAllEntries()
 }
