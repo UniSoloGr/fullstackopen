9192cec50345f4d0e8dbf0ffe50b0e932c147f7a
UniSoloGr <filipp.barkov@helsinki.fi>
2026-07-06 12:09:03 +0300

    feat: do exercise 4.18

--- part4/blogilista/backend/app.js
+++ part4/blogilista/backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -26,6 +27,7 @@ console.log(database)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(errorHandler)
 
